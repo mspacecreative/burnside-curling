b302689ba4225b08b3d56d72be6e55acbe2763c9
Matt <matt@mspacecreative.com>
2017-09-19 11:54:36 -0300

    a

--- wp-content/themes/burnsidecc/archive.php
+++ wp-content/themes/burnsidecc/archive.php
@@ -1,19 +1,21 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
-
-			<h1><?php _e( 'Archives', 'html5blank' ); ?></h1>
-
-			<?php get_template_part('loop'); ?>
-
-			<?php get_template_part('pagination'); ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+	<div class="post-container clearfix">
+		<main role="main">
+				<!-- section -->
+				<section>
+		
+					<h1><?php _e( 'Archives', 'html5blank' ); ?></h1>
+		
+					<?php get_template_part('loop'); ?>
+		
+					<?php get_template_part('pagination'); ?>
+		
+				</section>
+				<!-- /section -->
+			</main>
+		
+		<?php get_sidebar(); ?>
+	</div>
 
 <?php get_footer(); ?>
