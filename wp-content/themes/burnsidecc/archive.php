7aea8ab32f0b7d9228f47f94cf94e6ffdc48b249
Matt <matt@mspacecreative.com>
2017-09-26 10:33:13 -0300

    c

--- wp-content/themes/burnsidecc/archive.php
+++ wp-content/themes/burnsidecc/archive.php
@@ -5,7 +5,7 @@
 				<!-- section -->
 				<section>
 		
-					<?php $post_date = get_the_date('F'); ?>
+					<?php $post_date = get_the_date('F, Y'); ?>
 					<h1><?php echo $post_date; ?><?php _e( ' Archive', 'html5blank' ); ?></h1>
 		
 					<?php get_template_part('loop'); ?>
